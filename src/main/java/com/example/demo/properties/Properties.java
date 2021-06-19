64008daaa563c652bcbeb70cf44efd15d1d97812
黄敬旺 <1023668958@qq.com>
2021-06-20 00:15:36 +0800

    新增了python服务器的地址和图像的分类

--- src/main/java/com/example/demo/properties/Properties.java
+++ src/main/java/com/example/demo/properties/Properties.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 /**
  * 配置类暂时包含各种配置
@@ -29,8 +31,11 @@ public class Properties {
   @Value("#{0.85}")
   private double confidence;
 
-  @Value("localhost:8899/imgCfy")
+  @Value("http://192.168.43.184:8899/imgCfy")
   private String AIURL;
+
+  @Value("#{'人物,动物,自然风景,交通工具,建筑,others'.split(',')}")
+  private List<String> imgCategy;
 
   public Properties() {
   }
